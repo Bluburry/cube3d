9a879d17bea7600945fa27483f0817f754700f50
jegger <jpedroegger@gmail.com>
2024-02-22 22:50:36 +0000

    floor and ceiling RGB working

--- src/cube.c
+++ src/cube.c
@@ -13,28 +13,25 @@ int	main(int ac, char **av)
 	init_map(&data);
 	read_file(av[1], &data);
 	if (validate_map(&data))
+	{
 		printf("Invalid Map.\n");
+		return (1);
+	}
 	get_player_position(&data);
 
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Cub3D");
-	mlx_key_hook(data.win, close_window, &data);
+	// mlx_key_hook(data.win, close_window, &data);
 	mlx_hook(data.win, 17, 1L << 17, &on_destroy, &data);
+	mlx_hook(data.win, 2, 1L << 0, &close_window, &data);
 
-	data.player.pos.x = 26.5;
-	data.player.pos.y = 12.5;
-	data.player.dir.x = 0;
-	data.player.dir.y = -1;
-	data.player.plane.x = 0.6;
-	data.player.plane.y = 0;
 	raycast_attempt(&data);
-	//init_minimap(&data);
-	//draw_lines(&data);
-	//draw_rectangles(&data);
-	//draw_player(&data);
+	// init_minimap(&data);
+	// draw_rectangles(&data);
+	// draw_player(&data);
 
-	mlx_put_image_to_window(data.mlx, data.win, 
-		data.minimap.img.mlx_img, 0, 0);
+	// mlx_put_image_to_window(data.mlx, data.win, 
+	// 	data.minimap.img.mlx_img, 0, 0);
 	mlx_loop(data.mlx);
 	return (0);
 }
--- src/map/read_file.c
+++ src/map/read_file.c
@@ -1,5 +1,73 @@
 #include "cube.h"
 
+unsigned long createRGB(char *sub_str)
+{
+    char    **values;
+    int     RGB[3];
+    auto int     i = 0;
+
+    values = ft_split(sub_str, ',');
+    while (values[i] != 0)
+        i++;
+    if (i != 3)
+    {
+        printf("RGB wrong amount of values!\n");
+        exit(0);
+    }
+    i = -1;
+    while (values[++i])
+    {
+        RGB[i] = ft_atoi(values[i]);
+        if (RGB[i] > 255)
+        {
+            printf("RGB wrong value!\n");
+            exit(0);
+        }
+    }
+    free(values);
+    return ((RGB[0] & 0xff) << 16) + ((RGB[1] & 0xff) << 8) + (RGB[2] & 0xff);
+}
+
+int		check_commas(char *str)
+{
+	auto int	i = 0, j = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+		{
+			if (str[i] != ',')
+			{
+				printf("RGB - Not a comma [%C]!\n", str[i]);
+				return 1;
+			}
+			j++;
+		}
+		i++;
+	}
+	if (j != 2)
+	{
+		printf("RGB - Wrong amount of commas!\n");
+		return 1;
+	}
+	return 0;
+}
+
+void	save_colours(char *str, t_data *data)
+{
+	char		t;
+	auto char	*sub_str = NULL;
+
+	sub_str = ft_substr(str, 2, ft_strlen(str) - 3);
+	if (check_commas(sub_str))
+		exit(0);	
+	t = str[0];	
+	if (t == 'F')
+		data->new_map.floor = createRGB(sub_str);
+	else if (t == 'C')
+		data->new_map.sky = createRGB(sub_str);
+	free(sub_str);
+}
+
 void	read_file(char *file, t_data *data)
 {
 	char	*ptrs;
@@ -16,11 +84,21 @@ void	read_file(char *file, t_data *data)
 				|| !ft_strncmp(ptrs, "WE", 2) || !ft_strncmp(ptrs, "EA", 2))
 			save_texture_path(ptrs, data);
 		else if (!ft_strncmp(ptrs, "F", 1) || !ft_strncmp(ptrs, "C", 1))
-			continue ;
-		else if (!ft_strchr(ptrs, '1'))
-			continue ;
-		else
+			save_colours(ptrs, data);
+		else if (ft_strchr(ptrs, '1'))
+		{
+			if (!data->new_map.no_path || !data->new_map.so_path || \
+				!data->new_map.we_path || !data->new_map.ea_path || \
+				!data->new_map.floor || !data->new_map.sky)
+			{
+				free(ptrs);
+				printf("Missing texture or RGB\n");
+				exit(0) ;
+			}
 			save_map(ptrs, data);
+		}
+		else
+			printf("PTRS: %s", ptrs);
 		free(ptrs);
 	}
 	data->new_map.map[data->new_map.rows] = 0; // Is it ok!?
